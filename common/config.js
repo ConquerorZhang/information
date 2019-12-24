eefad633042dd318d888d3898067bf018025461d
zhangshuaiteng <zhangshuaiteng@163.com>
2019-12-24 16:49:00 +0800

    测试用

--- common/config.js
+++ common/config.js
@@ -4,6 +4,6 @@ module.exports = {
   // verCode: '191126001',
   imageURL: "http://10.10.5.33:8888/file/fileupload?type=image",
   httpURL: "https://www.baidu.com",
-  testURL: "http://10.10.5.33:80/api",
+  testURL: "http://skyfly-hf.6655.la:30333/api", 
   subDomain: "mall",
 }

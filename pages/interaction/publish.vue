fe45b76185ea6b51a0c7b2d0659cc36892be2d0e
zhangcong <1844761074@qq.com>
2019-12-26 10:06:13 +0800

    修改bug

--- pages/interaction/publish.vue
+++ pages/interaction/publish.vue
@@ -117,10 +117,10 @@
 				let imageURL;
 				if (process.env.NODE_ENV === 'development') {
 					// console.log(' 开发环境')
-					http = CONFIG.offlineImageURL;
+					imageURL = CONFIG.offlineImageURL;
 				} else {
 					// console.log('生产环境')
-					http = CONFIG.offlineImageURL;
+					imageURL = CONFIG.offlineImageURL;
 				}
 				uni.chooseImage({
 					sourceType: sourceType[this.sourceTypeIndex],

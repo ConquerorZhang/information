bce20e656df791cdfef339d085bbbd366a45b7ca
zhangcong <1844761074@qq.com>
2020-03-31 17:43:08 +0800

    修改https

--- common/config.js
+++ common/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   offlineImageURL: "http://116.239.32.177:8080/fileupload/file/fileupload?type=image",
   onlineImageURL: "http://116.239.32.177:8080/fileupload/file/fileupload?type=image",
-  httpURL: "http://116.239.32.178:8080/cpk/api", 
-  testURL: "http://116.239.32.178:8080/cpk/api", 
+  httpURL: "https://116.239.32.178:8443/cpk/api", 
+  testURL: "https://116.239.32.178:8443/cpk/api", 
   subDomain: "mall",
 }

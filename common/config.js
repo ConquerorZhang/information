7d778a5337e0fac254a42ed7747f9c310aaac563
zhangcong <1844761074@qq.com>
2020-04-01 10:24:56 +0800

    更新上传域名

--- common/config.js
+++ common/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  offlineImageURL: "http://116.239.32.177:8080/fileupload/file/fileupload?type=image",
-  onlineImageURL: "http://116.239.32.177:8080/fileupload/file/fileupload?type=image",
+  offlineImageURL: "https://116.239.32.177:9443/fileupload/file/fileupload?type=image",
+  onlineImageURL: "https://116.239.32.177:9443/fileupload/file/fileupload?type=image",
   httpURL: "https://116.239.32.178:8443/cpk/api", 
   testURL: "https://116.239.32.178:8443/cpk/api", 
   subDomain: "mall",
